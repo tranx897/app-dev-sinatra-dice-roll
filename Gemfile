dddc815948ed3305238ca02b16190a5c4c9ef086
tranx897 <63356332+tranx897@users.noreply.github.com>
2024-04-10 15:11:58 +0000

    Dice Roll Complete

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# /Gemfile
+source "https://rubygems.org"
+
+gem "puma"
+gem "sinatra"
+
+gem "rackup", "~> 2.1"
+gem "sinatra-contrib"
+gem "better_errors"
+gem "binding_of_caller"
---
+++ dice.rb
@@ -0,0 +1,75 @@
+# /dice.rb
+
+require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+get("/") do
+  erb(:elephant) # refers to elephant.erb view
+end
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  @rolls = []
+  2.times do
+  @die = rand(1..6)
+  @rolls.push(die)
+  end
+	
+  erb(:two_six) # erb assumes the file layout in views folder is the default layout file
+end
+
+get("/dice/2/10") do
+  @rolls = []
+  2.times do
+    @die = rand(1..10)
+    @rolls.push(die)
+  end
+	
+  erb(:two_ten)
+end
+
+get("/dice/1/20") do
+  @die = rand(1..20)
+	
+	@outcome = "You rolled a #{die}."
+	
+	erb(:one_twenty)
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+	second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die
+	
+	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	
+	erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
+end
